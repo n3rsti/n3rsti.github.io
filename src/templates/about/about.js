ee8b0d63aeb14da5cd5fa3f9797106c173d06dae
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2019-07-09 12:06:20 +0200

    fixed letter bug

--- src/templates/about/about.js
+++ src/templates/about/about.js
@@ -7,17 +7,17 @@ let date = new Date()
 let time = date.getFullYear() - 2018
 let content;
 if(time > 1){
-  content = time + "years"
+  content = time + " years"
 }
 else {
-  content = time + "year"
+  content = time + " year"
 }
 
 const About = () => (
     <StyledSection black auto min="true" id="about-me">
     <StyledIcon className="fas fa-user"></StyledIcon>
     <StyledAbout>Hello, I'M Krzysztof Witucki and I'M learning to become <b>Python Developer</b>. I've been coding
-      for about {content} year mainly in web technologies and <b>Python</b> and this is my Portfolio with my own projects. More project you can see
+      for about {content} mainly in web technologies and <b>Python</b> and this is my Portfolio with my own projects. More project you can see
       on my <a href="https://github.com/n3rsti" target="_blank" aria-label="Check out my github profile" title="Github profile">Github</a>.
       </StyledAbout>
     <StyledArrow href="#stack" aria-label="My proggraming skills section" title="Technology stack">
