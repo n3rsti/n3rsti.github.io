e20f8765990c0685f5bd80582f342d03e31d5865
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-04-17 23:06:01 +0200

    update fontawesome icon pack link

--- src/components/SEO/seo.js
+++ src/components/SEO/seo.js
@@ -10,8 +10,7 @@ const SEO = () => (
         <meta name="google-site-verification" content="3gQS-Jm4jngZiOxDesqjDU_rf6ljjjhKUOfBHgCFiOE" />
         <link rel="icon" href={logo} type="image/gif" sizes="16x16" />
         {/* Logo created with design evo */}
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-        crossorigin="anonymous"/>
+        <script src="https://kit.fontawesome.com/52781d0c34.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet"></link>
         <meta name="theme-color" content="#f1c40f"></meta>
         <title>Krzysztof Witucki</title>
